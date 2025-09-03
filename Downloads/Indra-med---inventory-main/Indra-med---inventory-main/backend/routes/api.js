d7a16d3b96180e9f5f8f00cb006c53b9be4a0b08
fly-hii <flyhii.tech@gmail.com>
2025-09-03 15:39:06 +0530

    Delete Downloads directory

--- Downloads/Indra-med---inventory-main/Indra-med---inventory-main/backend/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getData } = require('../controllers/databasecontroller');
-
-const router = express.Router();
-
-// API route to fetch data
-router.get('/data', getData);
-
-module.exports = router;
